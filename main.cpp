b1883c0bba310d3227079888aafe20ccb15a4aea
Paulo César Moraes <105931834+pcmoraesmenezes@users.noreply.github.com>
2022-11-07 13:58:59 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,88 +1,73 @@
-//IMPLEMENTAÇÃO DE LISTA SEQUENCIAL
 #include <iostream>
-#include <stdio.h>
 using namespace std;
-typedef struct no_ { //usa o typedef para colocar o nome no final, usa-se o "no" na chamda
-    int valor; // criação de variavel
-    int chave;
-}no; // tipo
-no bd[50]; // vetor da struct
-int TAM = 0; // tamanho do vetor
-int MAX = 50; // tamanho maximo do vetor
-int busca(int chave)// a função e inteira pois retorna o valor que voce esta procurando 
+typedef struct no_ // criação de uma struct que ficara responsavel pela chave e pelo valor
 {
-    int busca = -1; // caso não encontre nada, retorna -1
-    int i = 0; // variavel de busca
-    while(i < TAM ); //repetição para percorrer o tamanho do vetor
+int chave;
+int valor;
+}no;
+no bd[50]; // criação de um vetor da struct
+int tam = 0;
+int busca(int chave) // uma função de busca recebe como parametros a chave
+{
+    int retorno = 0; // o codigo ira retornar 0, caso a busca não encontre e retornara 1 caso encontre
+   for(int i = 0; i<= tam; i++) // a variavel i ira percorrer todo o tamanho do vetor
+   {
+     if(bd[i].chave == chave) // comparação entre as chaves
+     retorno = 1;
+   }
+   return retorno;
+}
+int insere(int chave, int valor) // a função insere recebe como parametros a chave e o valor
+{
+   int retorno = 0;
+   if(tam <= 50) // seo tamanho for maior do que 50 a lista estará cheia
+   {
+     if(busca(chave) == 0) // a chave precisa retornar falso, pois quer dizer que não existe o elemento
+     {
+      bd[tam].chave = chave; // inserção da chave
+      bd[tam].valor = valor; // inserção do valor
+      tam++; // incremento do tamanho pois foi adicionado elementos
+      retorno = chave;
+     }
+   }    
+   return retorno;
+}
+void imprime() // uma função simples para imprimir o vetor
+{
+    for(int i = 0; i < tam; i++)
     {
-        if(bd[i].chave == chave) // condição na qual compara a chave do vetor com a chave desejada pelo usuario
-        {
-            busca = i; // posição do valor desejado 
-            i = TAM+1; // encerrar a função
-        }
-        else {
-            i++; // continuar percorrendo o vetor
-        }
-        return busca; //retorna i caso encontre e -1 caso não encontre
-        
+        printf("Valor: %d | Chave: %d \n", bd[i].valor, bd[i].chave);
     }
+    cout << "----------------------------------" << endl;
 }
-int insere(int chave, int valor)
-{
-    int retorno = -1;
-    int x = busca(chave);
-    if(TAM < MAX){
-        if( x == -1){
-        bd[TAM].chave = chave;
-        bd[TAM].valor = valor;
-        TAM++;
-         retorno =  TAM;
-        }
-    else 
-        retorno = 0;
-    }
-    return retorno;
-}
-
-int remove(int chave)
-{
-  int teste = busca(chave);
-  int retorno = -1;
-  if(teste > -1) {
-    while(teste < TAM)
-      {
-        bd[teste].chave = bd[teste +1].chave;
-        bd[teste].valor = bd[teste + 1 ].valor;
-        teste++;
-      }
-    retorno = 1;
-    bd[teste].chave = 0;
-    bd[teste].valor = 0;
-    TAM --;
-  }
-  return retorno;
-}
-void imprime()
-{
-  for(int i = 0; i < TAM; i++)
-    {
-      printf("Valor:   %d / Chave:  %d\n", bd[i].valor, bd[i].chave);
-      
-    }
-  printf("----------------------------\n");
-}
-int main()
-{      
+int remove(int chave) // função remove recebe como parametro apenas a chave
+{   int retorno = 0;
    
-    printf("Valor inserido: %d\n", insere(1,2));
-    imprime();
-   remove(1);
-    printf("Valor inserido: %d\n", insere(2,5));
-  imprime();
-    printf("Valor inserido: %d\n", insere(3,3));
-  imprime();
-    printf("Valor inserido: %d\n", insere(5,4));
-  imprime();
-
-  
+        if(busca(chave) == 1 ) // a busca nesse caso precisa retornar TRUE pois quer dizer que o elemento existe
+        {
+            printf("entrou");
+            for(int i = 0; i<= tam; i++) 
+            {
+                if(bd[i].chave == chave)
+                {
+                    bd[i].chave = NULL; // apontando a chave para nulo
+                    tam = tam - 1; // decrementando o tamanho
+                    retorno = chave;
+                }
+            }
+        }
+     return retorno;
+    }
+   
+int main() // chamada das funções
+{
+   printf("Chave retornada : %d \n", insere(1,2));
+   imprime();
+   printf("Chave retornada : %d \n", insere(2,7));
+   remove(2);
+   imprime();
+   printf("Chave retornada : %d \n", insere(3,8));
+   imprime();
+   printf("Chave retornada : %d \n", insere(4,9));
+   imprime();
 }
